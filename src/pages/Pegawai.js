a302f3ebb2bb49c2fa410ff2300267f9b8257a1e
Windu Purnomo <wpwindupurnomo@gmail.com>
2019-04-04 15:09:00 +0700

    submit form

--- src/pages/Pegawai.js
+++ src/pages/Pegawai.js
@@ -2,7 +2,12 @@ import React, { useState, Fragment } from "react";
 import { Row, Col, FormGroup, Label, Input, Button, Table } from "reactstrap";
 
 export default props => {
-  const listPegawai = [
+  const [name, setName] = useState("");
+  const [phone, setPhone] = useState("");
+  const [email, setEmail] = useState("");
+  const [birthDate, setBirthDate] = useState("");
+  const [address, setAddress] = useState("");
+  const [listPegawai, setListPegawai] = useState([
     {
       name: "Zinedine Zidane",
       phone: "09878787878",
@@ -17,7 +22,7 @@ export default props => {
       birthDate: "2000/04/10",
       address: "Tasikmalaya"
     }
-  ];
+  ]);
   const cTr = listPegawai.map((p, i) => {
     return (
       <tr key={i}>
@@ -29,31 +34,74 @@ export default props => {
       </tr>
     );
   });
+  const submit = e => {
+    e.preventDefault();
+    setName("");
+    setPhone("");
+    setEmail("");
+    setBirthDate("");
+    setAddress("");
+
+    const pegawai = {
+      name: name,
+      phone: phone,
+      email: email,
+      birthDate: birthDate,
+      address: address
+    };
+
+    setListPegawai([...listPegawai, pegawai]);
+  };
   return (
     <Fragment>
       <h1>Data Pegawai</h1>
       <Row>
         <Col md={4}>
-          <form>
+          <form onSubmit={submit}>
             <FormGroup>
               <Label>Nama</Label>
-              <Input type="text" placeholder="Nama Pegawai" />
+              <Input
+                value={name}
+                onChange={e => setName(e.target.value)}
+                type="text"
+                placeholder="Nama Pegawai"
+              />
             </FormGroup>
             <FormGroup>
               <Label>Telepon</Label>
-              <Input type="text" placeholder="No. Telepon Pegawai" />
+              <Input
+                value={phone}
+                onChange={e => setPhone(e.target.value)}
+                type="text"
+                placeholder="No. Telepon Pegawai"
+              />
             </FormGroup>
             <FormGroup>
               <Label>Email</Label>
-              <Input type="text" placeholder="Alamat Email Pegawai" />
+              <Input
+                value={email}
+                onChange={e => setEmail(e.target.value)}
+                type="text"
+                placeholder="Alamat Email Pegawai"
+              />
             </FormGroup>
             <FormGroup>
               <Label>Tanggal Lahir</Label>
-              <Input type="date" placeholder="Tanggal Lahir Pegawai" />
+              <Input
+                value={birthDate}
+                onChange={e => setBirthDate(e.target.value)}
+                type="date"
+                placeholder="Tanggal Lahir Pegawai"
+              />
             </FormGroup>
             <FormGroup>
               <Label>Alamat</Label>
-              <Input type="textarea" placeholder="Alamat Pegawai" />
+              <Input
+                value={address}
+                onChange={e => setAddress(e.target.value)}
+                type="textarea"
+                placeholder="Alamat Pegawai"
+              />
             </FormGroup>
             <Button color="primary">Simpan</Button>
           </form>
